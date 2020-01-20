963dab59759e0944ce3e030e6c1088c2c8c9cf89
alex <alexandre.dasilva.martins33@gmail.com>
2020-01-20 17:30:36 +0100

    new

--- functions/edituser.php
+++ functions/edituser.php
@@ -1,13 +1,26 @@
 <?php
     echo "Edit User";
+
+    // 1 : connect to database
     require("database.php");
 
-    //ID
     //new pseudo
-    //Pour s'assurer que l'on recoit bien toute les informations via le formulaire
+    var_dump($_POST["newPseudo"]);
+    var_dump($_POST["user_id"]);
 
-    var_dump($_POST);
 
-    $req = $db->prepare("UPDATE users SET id = ? WHERE id = ?");
+    $req = $db->prepare("UPDATE users SET pseudo = :newPseudo WHERE id = :user_id");
 
-    $req->execute(["id"]);    
+
+    //bindParam (id, newPseudo)
+    $req->bindParam(":newPseudo" , $_POST["newPseudo"]);
+    $req->bindParam(":user_id" , $_POST["user_id"]);
+
+
+
+    
+    //execute
+    $req->execute();
+
+    //header Location : on redirige l'utilisateur vers la page profils
+    header("location:../profils.php");
